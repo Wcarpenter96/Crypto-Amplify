8a251546d69cd84c79b36d26c17e285cbeab0306
Weston Carpenter <wc.cc96@gmail.com>
2019-12-11 15:26:38 -0800

    amplify with auth

---
+++ src/graphql/queries.js
@@ -0,0 +1,66 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getTalk = `query GetTalk($id: ID!) {
+  getTalk(id: $id) {
+    id
+    clientId
+    name
+    description
+    speakerName
+    speakerBio
+    comments {
+      nextToken
+    }
+  }
+}
+`;
+export const listTalks = `query ListTalks(
+  $filter: ModelTalkFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listTalks(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      clientId
+      name
+      description
+      speakerName
+      speakerBio
+    }
+    nextToken
+  }
+}
+`;
+export const getComment = `query GetComment($id: ID!) {
+  getComment(id: $id) {
+    id
+    message
+    createdBy
+    talk {
+      id
+      clientId
+      name
+      description
+      speakerName
+      speakerBio
+    }
+  }
+}
+`;
+export const listComments = `query ListComments(
+  $filter: ModelCommentFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listComments(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      message
+      createdBy
+    }
+    nextToken
+  }
+}
+`;
